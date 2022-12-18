826270b4cdb93bfcb719e5e8fe7eddfea2518eef
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-12-18 18:15:53 +0300

    tasks

--- newTasks/src/ru/mirea/lab2/Task2.java
+++ newTasks/src/ru/mirea/lab2/Task2.java
@@ -4,7 +4,6 @@ public class Task2 {
     public static void main(String[] args){
         TestAuthor testAuthor = new TestAuthor();
         TestBall testBall = new TestBall();
-        testAuthor.test();
         testBall.test();
 
         TestCircle testPointAndCircle = new TestCircle();
--- newTasks/src/ru/mirea/lab4/Computer.java
+++ newTasks/src/ru/mirea/lab4/Computer.java
@@ -1,5 +1,8 @@
 package ru.mirea.lab4;
 
+import ru.mirea.lab29.Dish;
+
+import java.util.Arrays;
 import java.util.Random;
 
 enum Mark {
@@ -23,27 +26,61 @@ public class Computer {
 
     @Override
     public String toString() {
-        return "Computer{" +
-                "processor=" + processor +
-                ", memory=" + memory +
-                ", monitor=" + monitor +
-                ", mark=" + mark +
-                '}';
+        return "Computer\n{" +
+                "\nprocessor=" + processor.toString() +
+                "\nmemory=" + memory.toString() +
+                "\nmonitor=" + monitor.toString() +
+                "\nmark=" + mark.toString() +
+                "\n}";
+    }
+
+    public static void main(String[] args){
+        Processor cpu = new Processor();
+        Memory storage = new Memory();
+        Monitor display = new Monitor();
+        Mark mark = Mark.IBM;
+        Computer comp = new Computer(cpu, storage, display, mark);
+        System.out.println(comp.toString());
     }
 }
 
 class Processor {
-    String name;
-    Integer cores;
-    Integer clockSpeed;
+    String name = "Intel";
+    Integer cores = 8;
+    Integer clockSpeed = 4200;
+
+    @Override
+    public String toString() {
+        return "Processor{" +
+                "name='" + name + '\'' +
+                ", cores=" + cores +
+                ", clockSpeed=" + clockSpeed +
+                '}';
+    }
 }
 
 class Memory {
-    Integer ramCapacity;
-    Integer romCapacity;
+    Integer ramCapacity = 16;
+    Integer romCapacity = 512;
+
+    @Override
+    public String toString() {
+        return "Memory{" +
+                "ramCapacity=" + ramCapacity +
+                ", romCapacity=" + romCapacity +
+                '}';
+    }
 }
 
 class Monitor {
-    Double size;
-    Integer resolution;
+    Double size = 27.;
+    Integer resolution = 2160;
+
+    @Override
+    public String toString() {
+        return "Monitor{" +
+                "size=" + size +
+                ", resolution=" + resolution +
+                '}';
+    }
 }
